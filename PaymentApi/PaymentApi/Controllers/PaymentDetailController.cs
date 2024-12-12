01b0dc816761103a411549faa3a63e5c29faf2f0
Wael-Bns <w.waelbenslimene004@gmail.com>
2024-12-12 11:50:18 +0100

    creating api and communicating with the angular app service

---
+++ PaymentApi/PaymentApi/Controllers/PaymentDetailController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentApi.Models;
+
+namespace PaymentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        private readonly PaymentDetailContext _context;
+
+        public PaymentDetailController(PaymentDetailContext context)
+        {
+            _context = context; 
+        }
+        //GET : api/PaymentDetail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
+        {
+            if(_context.PaymentDetails == null)
+            {
+                return NotFound();
+            }
+            return await _context.PaymentDetails.ToListAsync();
+        }
+        //GET : api/PaymentDetail/5 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetail>> GetPaymentDetail(int id)
+        {
+            return await _context.PaymentDetails.FindAsync(id);
+        }
+        //DELETE : api/PaymentDetail/5
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> DeletePaymentDetail(int id)
+        {
+            try
+            {
+                var paymentdetail = await _context.PaymentDetails.FindAsync(id);
+                if (paymentdetail == null)
+                {
+                    return NotFound();
+                }
+                _context.PaymentDetails.Remove(paymentdetail);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        //POST : Add a new payment record
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
+        {
+            if (_context.PaymentDetails == null)
+            {
+                return Problem("PaymentDetails is null");
+            }
+            _context.PaymentDetails.Add(paymentDetail);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetPaymentDetail", new { id = paymentDetail.PaymentDetailId },paymentDetail);
+        } 
+
+    }
+}
---
+++ PaymentApi/PaymentApi/Migrations/20241210150901_First Migration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PaymentDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(16)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    SecurityCode = table.Column<string>(type: "nvarchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PaymentDetailId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
